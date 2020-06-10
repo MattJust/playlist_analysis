67bbe9074e4f7c4327ee8cba3dc7840e9e4d9e2d
Matthew Justin <12598083+MattJust@users.noreply.github.com>
2020-06-10 09:57:12 +0100

    Update get_playlists.py

--- get_playlists.py
+++ get_playlists.py
@@ -1,5 +1,3 @@
-# Get songs from PopConn
-
 import pandas as pd
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
@@ -9,6 +7,7 @@ import csv
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 #get songs from the Pop Connoisseur Playlist
+
 pl_id = 'spotify:playlist:5AvWMqi0lbzSPvDR6WWesu'
 offset = 0
 
@@ -16,7 +15,6 @@ while True:
     response = sp.playlist_tracks(pl_id, offset=offset, fields='items.track.id,total')
     pc_tracks = [x['track']['id'] for x in response['items']]
     print(pc_tracks)
-    # pprint(response['items'])
     offset = offset + len(response['items'])
     print(offset, "/", response['total'])
     s = 0
@@ -40,7 +38,6 @@ while True:
     response = sp.playlist_tracks(pl_id2, offset=offset, fields='items.track.id,total')
     bb_tracks = [x['track']['id'] for x in response['items']]
     print(bb_tracks)
-    # pprint(response['items'])
     offset = offset + len(response['items'])
     print(offset, ",", response['total'])
     with open('bb.csv', 'w', newline='') as csvfile:
@@ -52,39 +49,3 @@ while True:
             thewriter.writerow({'song_id': song})
     if len(response['items']) == 0:
         break
-
-# Analyse files for features
-#
-# from __future__ import print_function    # (at top of module)
-# from spotipy.oauth2 import SpotifyClientCredentials
-# import json
-# import spotipy
-# import time
-# import sys
-#
-#
-# client_credentials_manager = SpotifyClientCredentials()
-# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-# sp.trace = False
-#
-# if len(sys.argv) > 1:
-#     artist_name = ' '.join(sys.argv[1:])
-# else:
-#     artist_name = 'weezer'
-#
-# results = sp.search(q=artist_name, limit=50)
-# tids = []
-# for i, t in enumerate(results['tracks']['items']):
-#     print(' ', i, t['name'])
-#     tids.append(t['uri'])
-#
-# start = time.time()
-# features = sp.audio_features(tids)
-# delta = time.time() - start
-# for feature in features:
-#     print(json.dumps(feature, indent=4))
-#     print()
-#     analysis = sp._get(feature['analysis_url'])
-#     print(json.dumps(analysis, indent=4))
-#     print()
-# print("features retrieved in %.2f seconds" % (delta,))
